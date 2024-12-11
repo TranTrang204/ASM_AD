4d4ed9c73e1408abc145d38198bedbbc6752ac6d
TranTrang204 <155514671+TranTrang204@users.noreply.github.com>
2024-12-11 22:33:46 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DemoAssignment2"
+include(":app")
+ 
